a01e15c9c8af33ebe896fd3ec986775904cbe32d
dmillerw <dmillerw@gmail.com>
2013-11-02 10:52:57 -0700

    Fixed crash on shift-clicking upgrades in/out of IO block

--- common/com/dmillerw/remoteIO/inventory/ContainerIOUpgrade.java
+++ common/com/dmillerw/remoteIO/inventory/ContainerIOUpgrade.java
@@ -3,6 +3,7 @@ package com.dmillerw.remoteIO.inventory;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.Container;
 import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 
 import com.dmillerw.remoteIO.block.tile.TileEntityIO;
@@ -38,4 +39,37 @@ public class ContainerIOUpgrade extends Container {
 		return true;
 	}
 
+	@Override
+	public ItemStack transferStackInSlot(EntityPlayer player, int slotID) {
+		ItemStack itemstack = null;
+		Slot slot = (Slot) this.inventorySlots.get(slotID);
+
+		if (slot != null && slot.getHasStack()) {
+			ItemStack itemstack1 = slot.getStack();
+			itemstack = itemstack1.copy();
+
+			if (slotID >= 0 && slotID <= 9) {
+				if (!this.mergeItemStack(itemstack1, 10, 45, true)) {
+					return null;
+				}
+			} else if (!this.mergeItemStack(itemstack1, 0, 9, false)) {
+				return null;
+			}
+
+			if (itemstack1.stackSize == 0) {
+				slot.putStack((ItemStack) null);
+			} else {
+				slot.onSlotChanged();
+			}
+
+			if (itemstack1.stackSize == itemstack.stackSize) {
+				return null;
+			}
+
+			slot.onPickupFromSlot(player, itemstack1);
+		}
+
+		return itemstack;
+	}
+	
 }
