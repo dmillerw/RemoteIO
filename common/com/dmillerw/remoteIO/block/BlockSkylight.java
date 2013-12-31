c5d053149dac7fd72b394d4916927096bc3e30d5
Dylan Miller <dmillerw@gmail.com>
2013-12-30 23:54:20 -0800

    Tweaked skylight code, now carries state across all connected skylights

--- common/com/dmillerw/remoteIO/block/BlockSkylight.java
+++ common/com/dmillerw/remoteIO/block/BlockSkylight.java
@@ -1,15 +1,17 @@
 package com.dmillerw.remoteIO.block;
 
+import com.dmillerw.remoteIO.core.CreativeTabRIO;
+import com.dmillerw.remoteIO.core.IUpdatableBlock;
+import com.dmillerw.remoteIO.lib.ModInfo;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.util.Icon;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import net.minecraftforge.common.ForgeDirection;
 
-import com.dmillerw.remoteIO.core.CreativeTabRIO;
-import com.dmillerw.remoteIO.lib.ModInfo;
-
-public class BlockSkylight extends Block {
+public class BlockSkylight extends Block implements IUpdatableBlock {
 
 	private Icon[] icons;
 	
@@ -28,7 +30,7 @@ public class BlockSkylight extends Block {
 	}
 
 	public void onNeighborBlockChange(World world, int x, int y, int z, int causeID) {
-		if (!world.isRemote) {
+		if (!world.isRemote && causeID != this.blockID) {
 			updateState(world, x, y, z);
 		}
 	}
@@ -36,24 +38,72 @@ public class BlockSkylight extends Block {
 	private void updateState(World world, int x, int y, int z) {
 		int meta = world.getBlockMetadata(x, y, z);
 		boolean powered = world.isBlockIndirectlyGettingPowered(x, y, z);
+
+		if (powered && meta == 0) {
+			world.setBlockMetadataWithNotify(x, y, z, 1, 3);
+			world.updateAllLightTypes(x, y, z);
+			for (ForgeDirection side : ForgeDirection.VALID_DIRECTIONS) {
+				Block block = Block.blocksList[world.getBlockId(x + side.offsetX, y + side.offsetY, z + side.offsetZ)];
+				
+				if (block != null && block instanceof IUpdatableBlock) {
+					((IUpdatableBlock)block).onBlockUpdate(world, x + side.offsetX, y + side.offsetY, z + side.offsetZ, this.blockID, 1);
+				}
+			}
+		} else if (!powered && meta == 1) {
+			world.setBlockMetadataWithNotify(x, y, z, 0, 3);
+			world.updateAllLightTypes(x, y, z);
+			for (ForgeDirection side : ForgeDirection.VALID_DIRECTIONS) {
+				Block block = Block.blocksList[world.getBlockId(x + side.offsetX, y + side.offsetY, z + side.offsetZ)];
+				
+				if (block != null && block instanceof IUpdatableBlock) {
+					((IUpdatableBlock)block).onBlockUpdate(world, x + side.offsetX, y + side.offsetY, z + side.offsetZ, this.blockID, 0);
+				}
+			}
+		}
+	}
+
+	@Override
+	public void onBlockUpdate(World world, int x, int y, int z, int causeID, int causeMeta) {
+		int meta = world.getBlockMetadata(x, y, z);
+		boolean powered = causeMeta == 1;
 		
 		if (powered && meta == 0) {
 			world.setBlockMetadataWithNotify(x, y, z, 1, 3);
 			world.updateAllLightTypes(x, y, z);
+			for (ForgeDirection side : ForgeDirection.VALID_DIRECTIONS) {
+				Block block = Block.blocksList[world.getBlockId(x + side.offsetX, y + side.offsetY, z + side.offsetZ)];
+				
+				if (block != null && block instanceof IUpdatableBlock) {
+					((IUpdatableBlock)block).onBlockUpdate(world, x + side.offsetX, y + side.offsetY, z + side.offsetZ, this.blockID, 1);
+				}
+			}
 		} else if (!powered && meta == 1) {
 			world.setBlockMetadataWithNotify(x, y, z, 0, 3);
 			world.updateAllLightTypes(x, y, z);
+			for (ForgeDirection side : ForgeDirection.VALID_DIRECTIONS) {
+				Block block = Block.blocksList[world.getBlockId(x + side.offsetX, y + side.offsetY, z + side.offsetZ)];
+				
+				if (block != null && block instanceof IUpdatableBlock) {
+					((IUpdatableBlock)block).onBlockUpdate(world, x + side.offsetX, y + side.offsetY, z + side.offsetZ, this.blockID, 0);
+				}
+			}
 		}
+	}
+	
+	@Override
+	public boolean shouldSideBeRendered(IBlockAccess world, int x, int y, int z, int side) {
+		ForgeDirection forgeSide = ForgeDirection.getOrientation(side).getOpposite();
+		
+		int thisMeta = world.getBlockMetadata(x, y, z);
+		int thatMeta = world.getBlockMetadata(x + forgeSide.offsetX, y + forgeSide.offsetY, z + forgeSide.offsetZ);
+		int thatID = world.getBlockId(x + forgeSide.offsetX, y + forgeSide.offsetY, z + forgeSide.offsetZ);
+		
+		return !(thisMeta == 1 && (thatMeta == 1 && thatID == this.blockID));
 	}
 	
 	@Override
 	public boolean isOpaqueCube() {
         return false;
-    }
-	
-	@Override
-	public int getRenderBlockPass() {
-        return 1;
     }
 	
 	@Override
@@ -73,5 +123,5 @@ public class BlockSkylight extends Block {
 		this.icons[0] = register.registerIcon(ModInfo.RESOURCE_PREFIX + "blank");
 		this.icons[1] = register.registerIcon(ModInfo.RESOURCE_PREFIX + "other/glass");
 	}
-	
+
 }
