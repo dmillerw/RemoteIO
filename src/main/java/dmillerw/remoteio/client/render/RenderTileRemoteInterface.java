264b8b86d81d499deb142b25f539727f7e97784c
dmillerw <dmillerw@gmail.com>
2014-05-10 20:28:23 -0700

    Fixed issues with rendering blocks that use the alpha pass

--- src/main/java/dmillerw/remoteio/client/render/RenderTileRemoteInterface.java
+++ src/main/java/dmillerw/remoteio/client/render/RenderTileRemoteInterface.java
@@ -4,8 +4,10 @@ import cpw.mods.fml.client.FMLClientHandler;
 import cpw.mods.fml.common.FMLLog;
 import dmillerw.remoteio.block.tile.TileRemoteInterface;
 import dmillerw.remoteio.lib.DimensionalCoords;
+import net.minecraft.block.Block;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.WorldClient;
+import net.minecraft.client.renderer.OpenGlHelper;
 import net.minecraft.client.renderer.RenderBlocks;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.texture.TextureMap;
@@ -45,19 +47,29 @@ public class RenderTileRemoteInterface extends TileEntitySpecialRenderer {
 			GL11.glTranslated(-0.5, 0, -0.5);
 			GL11.glTranslated(-0.0002, 0, 0); // To prevent ZFighting when rendering with simple camo
 
+			Block remote = there.getBlock(worldClient);
 
 			// Don't call the block's renderer if there's a simple camo chip
 			if (tile.visualState == TileRemoteInterface.VisualState.CAMOUFLAGE_REMOTE) {
 				GL11.glDisable(GL11.GL_LIGHTING);
+				OpenGlHelper.glBlendFunc(770, 771, 0, 1);
+				GL11.glEnable(GL11.GL_BLEND);
 
 				Tessellator.instance.startDrawingQuads();
 				Tessellator.instance.setColorOpaque_F(1, 1, 1);
 				Tessellator.instance.setTranslation(-tile.xCoord, -tile.yCoord, -tile.zCoord);
 				Tessellator.instance.addTranslation(-(there.x - tile.xCoord), -(there.y - tile.yCoord), -(there.z - tile.zCoord));
-				renderBlocks.renderBlockAllFaces(tile.getWorldObj().getBlock(there.x, there.y, there.z), there.x, there.y, there.z);
+
+				for (int i=0; i<2; i++) {
+					if (remote.canRenderInPass(i)) {
+						renderBlocks.renderBlockByRenderType(remote, there.x, there.y, there.z);
+					}
+				}
+
 				Tessellator.instance.setTranslation(0, 0, 0);
 				Tessellator.instance.draw();
 
+				GL11.glDisable(GL11.GL_BLEND);
 				GL11.glEnable(GL11.GL_LIGHTING);
 			}
 
