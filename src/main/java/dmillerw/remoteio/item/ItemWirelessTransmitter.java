095810fa4f39f28dd85868a20277fc894da6285e
dmillerw <dmillerw@gmail.com>
2014-05-17 17:21:55 -0700

    Wireless transmitter can now be bound to player

--- src/main/java/dmillerw/remoteio/item/ItemWirelessTransmitter.java
+++ src/main/java/dmillerw/remoteio/item/ItemWirelessTransmitter.java
@@ -9,11 +9,14 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.management.ServerConfigurationManager;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ChatComponentTranslation;
 import net.minecraft.util.IIcon;
 import net.minecraft.world.World;
 import net.minecraftforge.common.DimensionManager;
+import sun.net.www.content.text.plain;
 
 import java.util.List;
 
@@ -52,6 +55,18 @@ public class ItemWirelessTransmitter extends Item {
 		stack.setTagCompound(nbt);
 	}
 
+	public static void setPlayer(ItemStack stack, EntityPlayer player) {
+		if (!stack.hasTagCompound()) {
+			stack.setTagCompound(new NBTTagCompound());
+		}
+
+		NBTTagCompound nbt = stack.getTagCompound();
+
+		nbt.setString("player", player.getCommandSenderName());
+
+		stack.setTagCompound(nbt);
+	}
+
 	public static int getHitSide(ItemStack stack) {
 		if (!stack.hasTagCompound()) {
 			return -1;
@@ -82,6 +97,21 @@ public class ItemWirelessTransmitter extends Item {
 		return new float[] {hit.getFloat("x"), hit.getFloat("y"), hit.getFloat("z")};
 	}
 
+	public static EntityPlayer getPlayer(ItemStack stack) {
+		if (!stack.hasTagCompound()) {
+			return null;
+		}
+
+		NBTTagCompound nbt = stack.getTagCompound();
+
+		if (!nbt.hasKey("player")) {
+			return null;
+		}
+
+		ServerConfigurationManager manager = MinecraftServer.getServer().getConfigurationManager();
+		return manager.getPlayerForUsername(nbt.getString("player"));
+	}
+
 	private IIcon icon;
 
 	public ItemWirelessTransmitter() {
@@ -95,10 +125,16 @@ public class ItemWirelessTransmitter extends Item {
 	@Override
 	public void addInformation(ItemStack stack, EntityPlayer player, List list, boolean debug) {
 		DimensionalCoords coords = ItemLocationChip.getCoordinates(stack);
+		EntityPlayer bound = getPlayer(stack);
 
 		if (coords != null) {
 			list.add("Dimension: " + DimensionManager.getProvider(coords.dimensionID).getDimensionName());
 			list.add("X: " + coords.x + " Y: " + coords.y + " Z: " + coords.z);
+			list.add(" --- ");
+		}
+
+		if (bound != null) {
+			list.add("Bound to: " + bound.getDisplayName());
 		}
 	}
 
@@ -122,7 +158,9 @@ public class ItemWirelessTransmitter extends Item {
 	@Override
 	public ItemStack onItemRightClick(ItemStack stack, World world, EntityPlayer player) {
 		if (!world.isRemote) {
-			if (stack.hasTagCompound() && stack.getTagCompound().hasKey("position")) {
+			if (player.isSneaking()) {
+				setPlayer(stack, player);
+			} else if (stack.hasTagCompound() && stack.getTagCompound().hasKey("position")) {
 				DimensionalCoords coord = ItemLocationChip.getCoordinates(stack);
 				int side = getHitSide(stack);
 				float[] hit = getHitCoordinates(stack);
