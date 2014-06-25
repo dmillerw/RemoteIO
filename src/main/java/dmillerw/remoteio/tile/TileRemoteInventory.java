a0df6e4bb1334f6ba4682da4cf8f7a3a495165dc
dmillerw <dmillerw@gmail.com>
2014-06-24 18:07:16 -0700

    Bug fixes + bump to 1.0.5. Fixes #26, 27

--- src/main/java/dmillerw/remoteio/tile/TileRemoteInventory.java
+++ src/main/java/dmillerw/remoteio/tile/TileRemoteInventory.java
@@ -31,9 +31,10 @@ import net.minecraftforge.fluids.IFluidHandler;
 /**
  * @author dmillerw
  */
-@Optional.InterfaceList(
-		@Optional.Interface(iface = "ic2.api.energy.tile.IEnergySink", modid = "IC2")
-)
+@Optional.InterfaceList({
+	@Optional.Interface(iface = "ic2.api.energy.tile.IEnergyTile", modid = "IC2"),
+	@Optional.Interface(iface = "ic2.api.energy.tile.IEnergySink", modid = "IC2")
+})
 public class TileRemoteInventory extends TileIOCore implements IInventory, IFluidHandler, IEnergySink {
 
 	public static final byte ACCESS_INVENTORY = 0;
